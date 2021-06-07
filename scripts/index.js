721dd30ba6d47a82c09c9631717759919f486b3d
Nikolay Makarich <iproggram@yandex.ru>
2021-06-07 23:46:02 +1100

    feat: добавить адаптив под мобильные версии и планшеты

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,8 @@
-/*
-let nameAuthor = document.querySelector('.profile__author')
-
-let inputNameAuthor = document.querySelector('.popup__input_string_name-author');
-inputNameAuthor.value = nameAuthor;
-
-let typeOfActivity = "Исследователь океана"
-
-let inputTypeOfActivity = document.querySelector('.popup__input_string_type-of-activity');
-inputTypeOfActivity.value = typeOfActivity;
-*/
-/*let submitButton = document.querySelector('.popup__submit-button');----переменная должна распологаться ниже, надо перенести*/
-
-
 let openPopupButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closePopupButtom = document.querySelector('.popup__close-button');
-let closeButtom = document.querySelector('.popup__close-button');
 
-function togglePopup(event){
+function togglePopup(event) {
     popup.classList.toggle('popup_is-opened');
 }
 
@@ -25,57 +10,48 @@ openPopupButton.addEventListener('click', togglePopup);
 closePopupButtom.addEventListener('click', togglePopup);
 
 function handleOverlayClick(event) {
-    if (event.target === event.currentTarget){
+    if (event.target === event.currentTarget) {
         togglePopup(event);
     }
 }
 
 popup.addEventListener('click', handleOverlayClick);
 
-
-
-
-let like = document.querySelectorAll('.element__like-button');
-
-console.log(like);
-
-
-
-function likeActive(){
-    like[3].classList.toggle('element__like-button_active');
-}
-
-
-like[3].addEventListener('click', likeActive);
-
-
-/*
-
-
-let nameAuthor = document.getElementById('nameAuthor');
-let typeOfActivity = document.getElementById('typeOfActivity');
-
-
 let formElement = document.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__input_string_name-author');
-let jobInput = formElement.querySelector('.popup__input_string_type-of-activity');
+let nameInput = document.querySelector('.popup__input_string_name-author');
+let jobInput = document.querySelector('.popup__input_string_type-of-activity');
+let profileAuthor = document.querySelector('.profile__author');
+let profileTypeOfActivity = document.querySelector('.profile__type-of-activity');
 
-nameInput.value = nameAuthor.textContent;
-jobInput.value = typeOfActivity.textContent;
+nameInput.value = profileAuthor.textContent;
+jobInput.value = profileTypeOfActivity.textContent;
 
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
+    // Эта строчка отменяет стандартную отправку формы.
+    // Так мы можем определить свою логику отправки.
+    // О том, как это делать, расскажем позже.
     evt.preventDefault();
-
     // Получите значение полей jobInput и nameInput из свойства value
-    
+    nameInputContent = nameInput.value; 
+    jobInputContent = jobInput.value;
     // Выберите элементы, куда должны быть вставлены значения полей
-
+    profileAuthor.textContent = nameInputContent;
+    profileTypeOfActivity.textContent = jobInputContent;
     // Вставьте новые значения с помощью textContent
+    togglePopup(event);
 }
-
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
 
-console.log(nameInput);
+/*  пытаемся заставить работать лайки
+let like = document.querySelectorAll('.element__like-button');
+
+console.log(like);
+
+function likeActive(){
+    like[i].classList.toggle('element__like-button_active');
+}
+
+like[i].addEventListener('click', likeActive);
 */
