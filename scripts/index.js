c98c4d61f5227f555c099a5592581b1d4c5c68d9
Nikolay Makarich <iproggram@yandex.ru>
2021-07-12 15:19:26 +1100

    feat: добавить попап и оптимизировать работу попапов

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,69 @@
-let openPopupButton = document.querySelector('.profile__edit-button');
+let popupConteiner = document.querySelector('.popup__container');
+
+
+let openPopupEditButton = document.querySelector('.profile__edit-button');
+let openPopupAddButton = document.querySelector('.profile__add-button');
+let closePopupButtom = popupConteiner.querySelector('.popup__close-button');
+
 let popup = document.querySelector('.popup');
-let closePopupButtom = document.querySelector('.popup__close-button');
-let formElement = document.querySelector('.popup__content');
+let popupEditProfile = document.querySelector('.popup__edit-profile');
+let popupAddCards = document.querySelector('.popup__add-cards');
+
 let nameInput = document.querySelector('.popup__input_string_name-author');
 let jobInput = document.querySelector('.popup__input_string_type-of-activity');
+let placeInput = document.querySelector('.popup__input_string_name-place');
+let linkInput = document.querySelector('.popup__input_string_link');
+
+let formElement = document.querySelector('.popup__content');
 let profileAuthor = document.querySelector('.profile__author');
 let profileTypeOfActivity = document.querySelector('.profile__type-of-activity');
 
-let addButton = document.querySelector('.profile__add-button');
+
+
+
 
 const itemTemplateContent = document.querySelector('.element-template').content;
 const elements = document.querySelector('.elements');
-const elementImg = itemTemplateContent.querySelector('.element__image');
-const elementText = itemTemplateContent.querySelector('.element__text');
+let elementImg = itemTemplateContent.querySelector('.element__image');
+let elementText = itemTemplateContent.querySelector('.element__text');
 const element = itemTemplateContent.querySelector('.element');
 
-function togglePopup() {
-  popup.classList.toggle('popup_is-opened');
-  nameInput.value = profileAuthor.textContent;
-  jobInput.value = profileTypeOfActivity.textContent;
+
+nameInput.value = profileAuthor.textContent;
+jobInput.value = profileTypeOfActivity.textContent;
+
+
+
+function openPopup(event) {
+  if (event.target === openPopupEditButton) {
+    popupEditProfile.classList.add('popup_is-opened');
+  }
+  else if( event.target === openPopupAddButton) {
+    popupAddCards.classList.add('popup_is-opened');
 }
+
+}
+
+/*function closePopup(evt) {
+   {
+    evt.target.classList.remove('popup_is-opened');
+  }
+ 
+}
+*/const button = document.querySelector('button');
+button.addEventListener('click', function (evt) {
+
+    const eventTarget = evt.target;
+    eventTarget.setAttribute('disabled', true);
+    console.log(eventTarget);
+}); 
+
 
 function handleOverlayClick(event) {
   if (event.target === event.currentTarget) {
-    togglePopup(event);
+    closePopup(evt);
   }
-}
+}//--------------------------------------------------------------------------------------remove
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
@@ -33,7 +71,11 @@ function formSubmitHandler(evt) {
   jobInputContent = jobInput.value;
   profileAuthor.textContent = nameInputContent;
   profileTypeOfActivity.textContent = jobInputContent;
-  togglePopup(evt);
+  placeInputContent = placeInput.value;
+  linkInputContent = linkInput.value;
+  //elementText.textContent = placeInputContent;--------------------------------------доделать
+
+  closePopup(evt);//--------------------------------------------------------------------remove
 }
 
 const initialElements = [
@@ -77,7 +119,7 @@ function renderElements() {
 };
 
 renderElements();
-
+/*
 function addElement() {
 
   elementImg.src = initialElements[4].link;
@@ -88,11 +130,15 @@ function addElement() {
 
 
 }
+*/
+//-----------------------------------------------------------------переделать тогл попап
+openPopupAddButton.addEventListener('click', openPopup);
+//addButton.addEventListener('click', addElement);---------повесить на кнопку сохранитть
+openPopupEditButton.addEventListener('click', openPopup);
 
-
-
-addButton.addEventListener('click', addElement);
-openPopupButton.addEventListener('click', togglePopup);
-closePopupButtom.addEventListener('click', togglePopup);
-popup.addEventListener('click', handleOverlayClick);
+//closePopupButtom.addEventListener('click', closePopup);//-------------------------remove
+popupEditProfile.addEventListener('click', handleOverlayClick);
+//popupAddCards.addEventListener('click', handleOverlayClick);
 formElement.addEventListener('submit', formSubmitHandler);
+
+
